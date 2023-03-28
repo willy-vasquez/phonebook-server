27c486a5a872d2c6574c6c195f65adc8eb468267
Willyjvp <willy.vas.p@gmail.com>
2023-03-28 17:31:05 -0600

    adding FE

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status - :response-time ms :body'));
 
 app.use(express.json());
+app.use(express.static('build'));
 app.use(cors());
 
 let persons = [
